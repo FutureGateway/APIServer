b1d33092b96373977e11155add8e13d10a29643b
Marco Fargetta <marco.fargetta@ct.infn.it>
2016-09-02 16:13:49 +0200

    Create the skeleton for the first unit test

---
+++ src/test/java/it/infn/ct/futuregateway/apiserver/v1/InfrastructureServiceTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.infn.ct.futuregateway.apiserver.v1;
+
+import javax.persistence.EntityManagerFactory;
+import org.junit.Test;
+
+/**
+ *
+ * @author Marco Fargetta <marco.fargetta@ct.infn.it>
+ */
+public class InfrastructureServiceTest {
+
+    /**
+     * EMF.
+     */
+    EntityManagerFactory emf;
+
+    /**
+     * Build.
+     */
+    public InfrastructureServiceTest() {
+    }
+
+    /**
+     * Test of getInfraDetails method, of class InfrastructureService.
+     */
+    @Test
+    public void testGetInfraDetails() {
+    }
+
+    /**
+     * Test of deleteInfra method, of class InfrastructureService.
+     */
+    @Test
+    public void testDeleteInfra() {
+    }
+
+}
